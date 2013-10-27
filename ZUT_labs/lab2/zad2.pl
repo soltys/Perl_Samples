29833f21ddde54908582c2e62b4e5e6ae1754433
Paweł Sołtysiak <soltys@uznam.org>
2013-10-27 22:14:58 +0100

    added files for 2nd labs

---
+++ ZUT_labs/lab2/zad2.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+use 5.012;
+use warnings;
+my %log;
+while(<>){
+    chomp;
+    my @data = split(/,/);
+    if  (scalar(@data) == 3){
+    my($who,$ip,$port) = @data;
+    $log{$ip}++;
+    }
+}
+
+foreach(keys %log){
+say $_ . "\t" . $log{$_};
+}
+
